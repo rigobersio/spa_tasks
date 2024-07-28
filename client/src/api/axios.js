f7c92e48b0a81e8eaa80a556a5d4bde6192e1cf1
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-07-27 22:09:39 -0400

    (rigobersio)edit:AppExpress, axiosfrom; delete:confg(pequeños ajustes para prueba cors)

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const URL = "https://spa-tasks.onrender.com/api" //"localhost:4000/api"; 
+const URL = "https://spa-tasks.onrender.com/api"; //"localhost:4000/api"
 const instanceAxios = axios.create({
-    baseURL: URL,
-    withCredentials: true
+  baseURL: URL,
+  withCredentials: true
 });
 
 export default instanceAxios;
