4718b34dbbe7a37c7d9131a167e388aef5f5a72b
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-08-06 03:00:05 -0400

    (r)edit:main(depurado para probar sin toast)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+//import { ToastContainer } from 'react-toastify';
+//import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
-      <ToastContainer
+     {/*} <ToastContainer
         position="top-center"
         autoClose={5000}
         hideProgressBar={false}
@@ -21,7 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         draggable
         pauseOnHover
         theme="light"
-      />
+      />*/}
     </AuthProvider>
   </React.StrictMode>,
 );
