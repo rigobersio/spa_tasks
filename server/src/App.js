fd23068b37b2f33925b09ec79aaabb7077d5fb5c
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-08-07 15:18:42 -0400

    (rig)edit:AppB(delete comentario y Path)

--- server/src/App.js
+++ server/src/App.js
@@ -2,7 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-//import path from 'path';
+
 
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
