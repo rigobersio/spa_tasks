a343d9a9e6d970aca66c44b587b40381239f0891
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-08-08 14:25:01 -0400

    (r)(prueba lonal negativa&&es mejor new rama)

--- server/src/App.js
+++ server/src/App.js
@@ -8,7 +8,7 @@ import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
 
 
-const URL = process.env.FRONTEND_URL
+const URL = process.env.FRONTEND_URL;
 const app = express();
 app.use(cors({
   origin: URL,
