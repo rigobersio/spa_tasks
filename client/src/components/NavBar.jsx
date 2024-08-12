5bd7fde24374c721fa58db34e2f5b50dcee286a2
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-08-12 02:07:10 -0400

    (ri)edit.NavBar(ajuste de espacio entre elementos2.6.0)

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -65,8 +65,8 @@ const NavBar = () => {
           </button>
         </div>
         </div>
-        <div className="lg:flex items center justify-end font-normal hidden text-2xl ml-5">
-          <div className="flex">
+        <div className="lg:flex items center justify-end font-normal hidden text-2xl pl-5">
+          <div className="flex pl-2">
             <ul className="flex gap-8 text-[18]">
               <LinkRouter to="/">
                 <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">SPA-Tasks</li>
