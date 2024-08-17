1a6e9048b777fbf155eaf6b8daa50d9fbf39c9f3
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-08-16 20:28:00 -0400

    (rig)edit:modalConfPass(to castelian)

--- client/src/components/ConfirmPasswordModal.jsx
+++ client/src/components/ConfirmPasswordModal.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
-import { toast } from 'react-toastify';
+//import { toast } from 'react-toastify';
+
+import { useLanguage } from '../context/LanguageContext';
 
 const ConfirmPasswordModal = ({ onConfirm, onClose }) => {
+  const { castelian } = useLanguage();
+
   const { register, handleSubmit, formState: { errors } } = useForm();
 
   const onSubmit = (data) => {
@@ -12,10 +16,10 @@ const ConfirmPasswordModal = ({ onConfirm, onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-6 rounded shadow-md">
-        <h2 className="text-xl font-bold mb-4">Confirm Password</h2>
+        <h2 className="text-xl font-bold mb-4">{castelian ? "Confirmar Contraseña" : "Confirm Password"}</h2>
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
           <div className="flex flex-col">
-            <label htmlFor="password" className="text-lg font-medium">Password</label>
+            <label htmlFor="password" className="text-lg font-medium">{castelian ? "Contraseña" : "Password"}</label>
             <input
               id="password"
               type="password"
@@ -26,8 +30,8 @@ const ConfirmPasswordModal = ({ onConfirm, onClose }) => {
             {errors.password && <p className="text-red-600">{errors.password.message}</p>}
           </div>
           <div className="flex justify-end">
-            <button type="button" onClick={onClose} className="mr-4">Cancel</button>
-            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">Confirm</button>
+            <button type="button" onClick={onClose} className="mr-4">{castelian ? "Cancelar" : "Cancel"}</button>
+            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">{castelian ? "Confirmar" : "Confirm"}</button>
           </div>
         </form>
       </div>
