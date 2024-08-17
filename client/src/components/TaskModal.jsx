c709075d1cb3341ffe28e7d12fccb5b1a5477588
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-08-16 20:34:22 -0400

    (rig)edit:modalConfPass(to castelian)

--- client/src/components/TaskModal.jsx
+++ client/src/components/TaskModal.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { toast } from 'react-toastify';
+//import { toast } from 'react-toastify';
+
+import { useLanguage } from '../context/LanguageContext';
 
 const TaskModal = ({ task, onClose, onUpdate, onDelete }) => {
+  const { castelian } = useLanguage();
   const { register, handleSubmit, formState: { errors } } = useForm({
     defaultValues: {
       title: task.title,
@@ -27,18 +30,18 @@ const TaskModal = ({ task, onClose, onUpdate, onDelete }) => {
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-white p-6 rounded-md w-full sm:w-1/2">
-        <h2 className="text-2xl font-bold mb-4 text-center">Edit Task</h2>
+        <h2 className="text-2xl font-bold mb-4 text-center">{castelian ? "Editar Tarea" : "Edit Task"}</h2>
         {message && <p className="text-2xl font-bold lg:my-12 my-3 text-center">{message}</p>}
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="flex flex-col mx-1 sm:mx-6">
-            <label htmlFor="title" className="text-lg font-medium">Title</label>
+            <label htmlFor="title" className="text-lg font-medium">{castelian ? "Título" : "Title"}</label>
             <input
               className="border border-gray-300 text-yellow-800 px-4 py-2 rounded-md my-2"
               type="text"
               placeholder="Title"
               {...register("title", { required: true })}
             />
-            {errors.title && <p className="text-red-600">Title is required</p>}
+            {errors.title && <p className="text-red-600">{castelian ? "Título es obligatorio" : "Title is required"}</p>}
           </div>
           <div className="flex flex-col mx-1 sm:mx-6">
             <label htmlFor="description" className="text-lg font-medium">Description</label>
@@ -62,7 +65,6 @@ const TaskModal = ({ task, onClose, onUpdate, onDelete }) => {
             <button type="submit" className="bg-blue-500 text-white lg:py-2 py-1 px-4 rounded-md shadow-md hover:bg-blue-700 transition duration-300">Update Task</button>
             <button type="button" className="bg-red-500 text-white lg:py-2 py-1 px-4 rounded-md shadow-md hover:bg-red-700 transition duration-300 ml-4" onClick={() => {
               onDelete(task._id);
-              toast.success('Task deleted successfully!');
             }}>Delete Task</button>
             <button type="button" className="bg-gray-500 text-white lg:py-2 py-1 px-4 rounded-md shadow-md hover:bg-gray-700 transition duration-300 ml-4" onClick={onClose}>Cancel</button>
           </div>
