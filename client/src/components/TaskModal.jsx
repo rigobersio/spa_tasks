26e8e05cfffd8fcde00769a88f77e362c75256b0
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-09-11 00:00:00 -0300

    (r)edit:TaskModal(import toast)

--- client/src/components/TaskModal.jsx
+++ client/src/components/TaskModal.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-//import { toast } from 'react-toastify';
 
 import { useLanguage } from '../context/LanguageContext';
 
